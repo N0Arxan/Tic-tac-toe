73685dee2a669eb0cc521f5b72623c447ecd7121
Arshan Hassanzadeh <arshan.hassanzadeh@uvic.cat>
2024-12-03 19:08:07 +0100

    Batman New Start

--- main.py
+++ main.py
@@ -1,42 +1,24 @@
 import os
 import random
-
-board_num = [i for i in range(9)]
+board = [i for i in range(9)]
 l = 0
-def print_board ():
-    os.system('cls')
-    global board_num
-    print(f"{board_num[:3]}\n{board_num[3:6]}\n{board_num[6:]}",sep="|" )
+def print_board():
     
-def move_o ():
-    move = int(input("enter your move "))
-    global l 
-    while True:
-        if board_num[move] != "X" or board_num[move] != "O" :
-            board_num[move] = "O"
-            l =+ 1
-            break
-        else:
-            print("invalid input")
-            continue
-def move_x ():
-    global l 
+    global board
+    os.system("cls")
+    print(f"{board[:3]} \n{board[3:6]} \n{board[6:]}")
+    print("enter q any moment for quitting")
     
-    while True:
-        xo = [i for i in range(9)]
-        move = random.choice(xo)
-        if board_num[move] != "X" or board_num[move] != "O" :
-            board_num[move] = "X"
-            l =+ 1
-            break
-        else:
-            print("invalid input")
-            continue
+print_board()
 
-def main ():
-    while l != 9 :
-        print_board()
-        move_o()
-        move_x()
+def move_x():
+    move = int(input("Enter a num Between 1...9 for your move as X"))
+    
+    board[move]="x"
 
-main()
+for i in range(9):
+    print_board()
+    move_x()
+
+
+
