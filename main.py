c71c6f3e06093f36f2a4b5d735709c4356918379
Arshan Hassanzadeh <arshan.hassanzadeh@uvic.cat>
2024-12-09 20:44:31 +0100

    bemoola

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import random
 board = [i for i in range(9)]
 l = 0
+turn = True
 def print_board():
     
     global board
@@ -9,16 +10,37 @@ def print_board():
     print(f"{board[:3]} \n{board[3:6]} \n{board[6:]}")
     print("enter q any moment for quitting")
     
-print_board()
+
 
 def move_x():
-    move = int(input("Enter a num Between 1...9 for your move as X"))
-    
-    board[move]="x"
+    move = int(input("Enter a num Between 1...9 for your move as X : "))
+    global l
+    while True:
+        if board[move] != "x" and board[move] != "o":
+            board[move] = "x"
+            
+            l += 1
+            break
+        else:
+            move = int(input("Invalid Input Enter a num _: "))
 
-for i in range(9):
+def move_o():
+    move = random.randint(0,8)
+    global l
+    while True:
+        if board[move] != "x" and board[move] != "o":
+            board[move] = "o"
+            l += 1
+            break
+        else:
+            move = random.randint(0,8)
+while l <= 9:
     print_board()
     move_x()
+    move_o()
+    print(l)
+
+
 
 
 
